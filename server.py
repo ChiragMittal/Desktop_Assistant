5988bf260ff0485507807c57c2d00e16ed523c6d
Chirag Mittal <chiragmittal97@yahoo.co.in>
2018-02-13 13:56:55 +0530

    news added

--- server.py
+++ server.py
@@ -66,32 +66,32 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-       return {}
+    #query = data.get('query')
+    #if query is None:
+    #    return {}
 
-    result = query.get('results')
-    if result is None:
-       return {}
+    #result = query.get('results')
+    #if result is None:
+    #    return {}
 
-    channel = result.get('channel')
-    if channel is None:
-       return {}
+    #channel = result.get('channel')
+    #if channel is None:
+    #    return {}
 
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-       return {}
+    #item = channel.get('item')
+    #location = channel.get('location')
+    #units = channel.get('units')
+    #if (location is None) or (item is None) or (units is None):
+    #    return {}
 
-    condition = item.get('condition')
-    if condition is None:
-       return {}
+    #condition = item.get('condition')
+    #if condition is None:
+    #    return {}
 
-    print(json.dumps(item, indent=4))
+    # print(json.dumps(item, indent=4))
 
-   speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-            ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+#    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+#             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
     speech = data
     print("Response:")
     print(speech)
